dd239f151ef218c228c3eca1927f7e8160f33923
Arun Kumar <arun.kumar@aspiresys.com>
2025-02-21 17:57:50 +0530

    Testing

--- file/components/Testing/Counter.test.js
+++ file/components/Testing/Counter.test.js
@@ -1,34 +1,45 @@
 import '@testing-library/jest-dom';
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, act } from '@testing-library/react';
 import Counter from './Counter';
 
-beforeEach(() => {
-  render(<Counter />);
-});
-
-afterEach(()=>{
-  console.log("Test Cleanup after Each test");
-});
-
-test('renders initial count of 0', () => {
-  const countText = screen.getByText(/0/i);
-  expect(countText).toBeInTheDocument();
-});
-
-test('increments count when button is clicked', () => {
-  const button = screen.getByText('Increment');
-  fireEvent.click(button);
-
-  const countText = screen.getByText(/1/i);
-  expect(countText).toBeInTheDocument();
-});
-
-test('increments count twice when button is clicked twice', () => {
-  const button = screen.getByText('Increment');
-  fireEvent.click(button);
-  fireEvent.click(button);
-
-  const countText = screen.getByText(/2/i);
-  expect(countText).toBeInTheDocument();
-});
+describe("Counter Component", ()=>{
+  beforeEach(() => {
+    render(<Counter />);
+  });
+  
+  afterEach(()=>{
+    console.log("Test Cleanup after Each test");
+  });
+  
+  test('renders initial count of 0', () => {
+    const countText = screen.getByText(/0/i);
+    expect(countText).toBeInTheDocument();
+  });
+  
+  test('increments count when button is clicked', () => {
+    const button = screen.getByText('Increment');
+    act(()=>{
+      fireEvent.click(button);
+    });
+  
+    //assertion
+    const countText = screen.getByText(/1/i);
+    expect(countText).toBeInTheDocument();
+  });
+  
+  it('increments count twice when button is clicked twice', () => {
+    const button = screen.getByText('Increment');
+    act(()=>{
+      fireEvent.click(button);
+    });
+  
+    act(()=>{
+      fireEvent.click(button);
+    });
+  
+    const countText = screen.getByText(/2/i);
+    expect(countText).toBeInTheDocument();
+  });
+  
+})

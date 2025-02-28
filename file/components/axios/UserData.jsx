b6a73db29f9402ba5edef75ea9b88a8cfb3bb530
Arun Kumar <arun.kumar@aspiresys.com>
2025-02-28 18:06:50 +0530

    Axios patch()

--- file/components/axios/UserData.jsx
+++ file/components/axios/UserData.jsx
@@ -4,29 +4,62 @@ import React, { useEffect, useState } from 'react'
 export default function () {
 
     const [data, setData] = useState([]);
+    const [id, setId] = useState(0);
+    const [newName, setNewName] = useState('');
+    const [newEmail, setNewEmail] = useState('');
+
+
+    const fetchData = async () => {
+        try {
+            const response = await axios.get('http://localhost:3000/users');
+            setData(response.data);
+        }
+        catch (error) {
+            alert(error);
+        }
+    };
 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axios.get('http://localhost:3000/users');
-                setData(response.data);
-            }
-            catch (error) {
-                alert(error);
-            }
-        };
-
         fetchData();
-    }, []);
+    },[]);
 
     const handleDelete = async (userID) => {
         try {
             await axios.delete(`http://localhost:3000/users/${userID}`); //delete data in db.json
-            setData(data.filter((user)=>(user.id !== userID))); // filter the items(except deleted id) in data and update 
+            setData(data.filter((user) => (user.id !== userID))); // filter the items(except deleted id) in data and update 
             alert("User Deleted");
         }
         catch (error) {
             alert(error);
+        }
+    }
+
+    const handleSubmit = async(event)=>{
+        event.preventDefault();
+
+        if (!newName || !newEmail) {
+            alert('Enter username and email');
+            return;
+        }
+      
+        try {
+            const updatedUser = {
+                username: newName,
+                email: newEmail
+            };
+
+            await axios.patch(`http://localhost:3000/users/${id}`, updatedUser);
+            fetchData(); 
+            alert("User Updated");
+
+            setData(data.map((user) => (user.id === id ? { ...user, ...updatedUser } : user)));
+
+            setId(0);
+            setNewName('');
+            setNewEmail('');
+        }
+        catch (error) {
+            alert("Id does not exist:  " + error);
         }
     }
 
@@ -35,6 +68,7 @@ export default function () {
         <div>
             <table>
                 <thead>
+                    <th>ID</th>
                     <th>Name</th>
                     <th>Email</th>
                     <th>Delete</th>
@@ -43,14 +77,39 @@ export default function () {
                     {data.map(fields => {
                         return (
                             <tr key={fields.id}>
+                                <td>{fields.id}</td>
                                 <td>{fields.username}</td>
                                 <td>{fields.email}</td>
-                                <td><button onClick={()=>handleDelete(fields.id)}>Delete</button></td>
+                                <td><button onClick={() => handleDelete(fields.id)}>Delete</button></td>
                             </tr>
                         );
                     })}
                 </tbody>
             </table>
+
+            <br />
+
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td><label htmlFor="id">ID</label></td>
+                                <td><input type="number" id='id' value={id} onChange={(event)=>setId(event.target.value)}/></td>
+                            </tr>
+                            <tr>
+                                <td><label htmlFor="username">Username</label></td>
+                                <td><input type="text" id='username' value={newName} onChange={(event)=>setNewName(event.target.value)}/></td>
+                            </tr>
+                            <tr>
+                                <td><label htmlFor="email">Email</label></td>
+                                <td><input type="email" id='email' value={newEmail} onChange={(event)=>setNewEmail(event.target.value)}/></td>
+                            </tr>
+                            <button type='submit'>Update</button>
+                        </tbody>
+                    </table>
+                </form>
+            </div>
         </div>
     )
 }

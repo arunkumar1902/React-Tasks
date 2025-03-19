7b7078e63dd6c72e5c38bd6191a302b8c610c99f
Arun Kumar <arun.kumar@aspiresys.com>
2025-03-19 18:21:03 +0530

    JavaScript Array methods

--- file/components/Javascript/JSArray.js
+++ file/components/Javascript/JSArray.js
@@ -29,5 +29,77 @@ arr1.splice(2,0,"five","six","seven"); // add new elements from index 2
 const tospliced = arr1.toSpliced(0,2);
 console.log(tospliced); //creates new array without changing original array
 const slice = arr1.slice(1);//creates a new array, remove the 1st element
+console.log(newArr.indexOf(1,2)); //search & return the 1st index of the element , 2nd-where to start the search
+console.log(newArr.lastIndexOf(1,2)); //return the index of last occurence of the element
+console.log(newArr.includes(1)); //return boolean
+console.log(newArr.find(x=>x%2===0)); //returns the 1st element that passes the test func, returns undefined if not found
+console.log(newArr.findIndex(x=>x<10)); //return index of the 1st element that passes the test func, return -1 if not found
+console.log(newArr.findLast(x=>x<10));//return the 1st element that passes the test func, starts from end of the array
+console.log(newArr.findLastIndex(x=>x<10));//return the index of 1st element that passes the test func, finds from end of the array
 
 
+
+// array sorting
+console.log(newArr.sort());//sort the original array
+console.log(newArr.reverse());
+const sorted = newArr.toSorted(); //create a new array and store the sorted elements
+const reversed = newArr.toReversed();
+const arraynumbers = [1,10,4,2,8,5];
+arraynumbers.sort((a,b)=>a-b); //sort by assending order
+arraynumbers.sort((a,b)=>b-a); // sort by decending order
+const arraystring = ["a","m","k","h"];
+arraystring.sort(); //sort by accending order
+arraystring.reverse() //for decending, 1st sort and then reverse
+const numberString = [1,"z",10, "d", 4];
+numberString.sort((a,b)=>{
+    if(typeof a === 'number' && typeof b ==='number'){
+        return a-b;
+    }
+    if(typeof a === 'string' && typeof b === 'string'){
+        return a.localeCompare(b); //localcompare is used to sort only strings, compare the 2 elements
+    }
+    if(typeof a === 'number'){
+        return -1 //moves the num before string
+    }
+    return 1 //move string before num
+})
+
+console.log(Math.min.apply(null, arraynumbers)); //returns the min value(only for numbers)
+console.log(Math.max.apply(null, arraynumbers)); //returns the max value
+// apply() is used to send the array elements as individual argument to math.min method
+
+//array iteration methods
+newArr.forEach((val, index, arr)=>{// iterate every element in an array and can modify the original array based on condition
+    arr[index] = val + " foreach";
+})
+console.log(newArr);
+
+const maparray = newArr.map((val, index,arr)=>{
+    return val + " map";// iterate every element in an array and create new array
+})
+console.log(maparray);
+console.log(newArr);
+
+const filterarray = arraynumbers.filter((val)=>{
+    return val%2==0;// filter creates new array with elements that pass the test function
+});
+console.log(filterarray);
+const reducearray = arraynumbers.reduce((accumulator, value)=>{
+    return accumulator + " " + value;//reduce array into a single value, does not change the original array 
+})
+console.log(reducearray);
+const reduceright = arraynumbers.reduceRight((accumulator, value)=>{
+    return accumulator + " " + value;//reduce array from right to left
+})
+console.log(reduceright);
+console.log(arraynumbers.every((val)=>val%2==0));//return boolean, checks all elements are pass the test function
+console.log(arraynumbers.some((val)=>val%2==0));//return boolean if atleast 1 element pass the test function
+const text = " hello world ";
+console.log(Array.from(text, val => val + " !" ));//return an array
+const keys = Array.from(arraynumbers.keys());// keys return a new array iteratir object, contains index of an array
+console.log(keys);
+const entries = Array.from(arraynumbers.entries());//entries return with key/value pairs
+console.log(entries);
+const arraywith = arraynumbers.with(1,3);// update element without altering original, 1st parameter - index, 2nd - value
+console.log(arraywith);
+ 
